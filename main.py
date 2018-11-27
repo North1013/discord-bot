5a35b23635611a2ea6bd5dbeadb33caa65d00462
root <root@discord-bot.frog.internal>
2018-11-27 13:51:04 -0500

    added reading key from key.txt and some commands

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import discord
+#!/usr/local/bin/python3.6
 import asyncio
+import discord
+import random
 
 client = discord.Client()
 
@@ -9,8 +11,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == "ANIKI":
-        await client.send_message(message.channel, "ANIKI :HandsUp:")
+    if message.content.startswith("!aniki"):
+        await client.send_message(message.channel, "ANIKI <:HandsUp:510384211346063360>")
+    elif message.content.startswith("!music"):
+        await client.send_message(message.channel, "qwe")
+    elif message.content("!f"):
+        await client.send_message(message.channel, "<:PepeHands:517047131329003530> BILLY <:PepeHands:517047131329003530> BILLY https://youtu.be/ISJlNRRvT3g")
 
-
-client.run("key")
+with open("key.txt", "r") as keyfile:
+    key = keyfile.read().rstrip("\n")
+    client.run(key)
